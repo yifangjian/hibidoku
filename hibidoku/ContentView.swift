2e27ea1bf6feaefe85a397da9e1d9077bf0d65cf
Jian Yi-fang <yifangjian015@gmail.com>
2026-08-13 13:22:54 +0800

    Initial Commit

---
+++ hibidoku/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hibidoku
+//
+//  Created by Jian on 2026/8/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hibidoku/hibidokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  hibidokuApp.swift
+//  hibidoku
+//
+//  Created by Jian on 2026/8/13.
+//
+
+import SwiftUI
+
+@main
+struct hibidokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
